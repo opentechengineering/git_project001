9df90d23588388695b86f7fc944c2fc8c142e8b4
opentechengineering <69385460+opentechengineering@users.noreply.github.com>
2020-08-08 20:59:38 +0100

    Update FeatureClass.java
    SECOND COMMIT

--- src/first_package/FeatureClass.java
+++ src/first_package/FeatureClass.java
@@ -5,7 +5,7 @@ public class FeatureClass {
 	public static void main(String[] args) {
 		System.out.println(" This is my commit from my feature branch");
 		System.out.println(" This is the first commit from remote master branch(github)");
-
+		System.out.println(" This is the SECOND commit from remote master branch(github)");
 
 	}
 
