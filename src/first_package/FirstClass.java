5a05b4c452b33e22e4b73e88da9ff570e1fdfd82
feti <fkarakoc02@gmail.com>
2020-08-08 19:32:37 +0100

    last commit  on local master branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -6,5 +6,7 @@ public class FirstClass {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("i am on my local feti branch");
+		System.out.println(" i want to have conflict");
+		System.out.println("i want to have a 2nd conflict");
 	}
 }
